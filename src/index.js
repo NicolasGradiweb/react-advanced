472471836f68f8d8d321b8074c7376b9a12edb5a
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-14 22:26:31 -0500

    Add babel configuration in webpack

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render('Helloo world!!', document.body)
+ReactDOM.render(<h1>Helloo world!!</h1>, document.getElementById('app'))
