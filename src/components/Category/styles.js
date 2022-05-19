0358b53e8cdb9ee123c867ac11daa33bfc1899eb
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-18 22:50:47 -0500

    Create styled components for Anchor and Image

---
+++ src/components/Category/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Anchor = styled.a`
+  display: flex;
+  flex-direction: column;
+  width: 75px;
+  text-align: center;
+  text-decoration: none;
+`
+export const Image = styled.img`
+  height: 75px;
+  width: 75px;
+  border: 1px solid #ddd;
+  box-shadow: 0 10px 14px rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+`
