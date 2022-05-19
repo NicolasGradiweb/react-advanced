b855ee6dac1ba0a3dcd9df8eecfe57e8dfdfabae
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-18 22:50:35 -0500

    Create styled components for Anchor and Image

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Anchor, Image } from './styles'
 
 const DEFAULT_IMAGE = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0mS6x_Ro4JPjX5tNDpZGDA0_kAcfYLgm7Sg&usqp=CAU'
 
 export const Category = ({ cover = DEFAULT_IMAGE, path, emoji = '?' }) => (
-  <a href={path}>
-    <img src={cover} />
+  <Anchor href={path}>
+    <Image src={cover} />
     {emoji}
-  </a>
+  </Anchor>
 )
