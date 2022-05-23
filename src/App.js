07718b39aa96dfdafc30f233aa03015300e6299d
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-22 19:41:52 -0500

    Create PhotoList componetn

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { CategoriesList } from './components/CategoriesList/index.js'
-import { PhotoCard } from './components/PhotoCard/index.js'
+import { PhotoList } from './components/PhotoList/index.js'
 import { GlobalStyles } from './GlobalStyles.js'
 
 const App = () => (
   <>
     <GlobalStyles />
     <CategoriesList />
-    <PhotoCard />
+    <PhotoList />
   </>
 )
 
