f11b95257d5af359a4508818e2cc94b78ab26d29
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-22 20:46:08 -0500

    Add logo component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { CategoriesList } from './components/CategoriesList/index.js'
 import { PhotoList } from './components/PhotoList/index.js'
 import { GlobalStyles } from './GlobalStyles.js'
+import { Logo } from './components/Logo'
 
 const App = () => (
   <>
     <GlobalStyles />
+    <Logo />
     <CategoriesList />
     <PhotoList />
   </>
