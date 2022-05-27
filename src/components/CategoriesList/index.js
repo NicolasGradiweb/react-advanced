fb9b14fe6b383fc77801ab5b0cfefbaa1c008244
Nicolas Mosquera <nicolas.mosquera@gradiweb.com>
2022-05-26 21:21:47 -0500

    Add useEffect for scrolling

--- src/components/CategoriesList/index.js
+++ src/components/CategoriesList/index.js
@@ -2,20 +2,31 @@
 import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { Item, List } from './styles'
-// import db from '../../../api/db.json'
+import db from '../../../api/db.json'
 
 export const CategoriesList = () => {
-  const [categories, setCategories] = useState([])
+  // const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
 
+  // useEffect(() => {
+  //   fetch('https://petgram-server.midudev.now.sh/categories')
+  //     .then(res => res.json())
+  //     .then(response => setCategories(response))
+  // }, [])
   useEffect(() => {
-    fetch('https://petgram-server.midudev.now.sh/categories')
-      .then(res => res.json())
-      .then(response => setCategories(response))
-  }, [])
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
 
-  const renderList = () => (
-    <List>
-      {categories.map(category => (
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
+      {db.categories.map(category => (
         <Item key={category.id}>
           <Category
             {...category}
@@ -26,6 +37,9 @@ export const CategoriesList = () => {
   )
 
   return (
-    renderList()
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
